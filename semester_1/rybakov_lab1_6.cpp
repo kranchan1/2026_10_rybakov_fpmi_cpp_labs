783aab9f4d080a2e65dc0eb30413bb6f2f7088fe
kranchan1 <krybakov42@gmail.com>
2026-09-11 00:22:05 +0300

    Add files via upload

---
+++ semester_1/rybakov_lab1_6.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+
+int main()
+{
+	setlocale(LC_ALL, "rus");
+	int a, b, d;
+	std::cout << "Введите значение а: ";
+	std::cin >> a;
+	std::cout << "Введите значение b: ";
+	std::cin >> b;
+	std::cout << "Введите значение разности арифметической прогрессии d: ";
+	std::cin >> d;
+	for (int i = 1; a <= b; ++i) {
+		if ((a % 3) == 0) {
+			std::cout << a << ' ';
+		}
+		a += d;
+	}
+}
