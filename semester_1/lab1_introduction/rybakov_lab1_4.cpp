8a280f1756a32d21221ce8d7c1e6992e29a0283a
kranchan1 <krybakov42@gmail.com>
2026-09-10 19:54:16 +0300

    Add files via upload

---
+++ semester_1/lab1_introduction/rybakov_lab1_4.cpp
@@ -0,0 +1,22 @@
+﻿
+#include <iostream>
+int main() {
+	int num, a, b, c, d, e, f, Sum_abc, Sum_def;
+	setlocale(LC_ALL, "rus");
+	std::cout << "Введите шестизначное число, чтобы узнать является ли оно волшебным:";
+	std::cin >> num;
+	a = num / 100000;
+	b = (num % 100000) / 10000;
+	c = (num % 10000) / 1000;
+	d = (num % 1000) / 100;
+	e = (num % 100) / 10;
+	f = num % 10;
+	Sum_abc = a + b + c;
+	Sum_def = d + e + f;
+	if (Sum_abc == Sum_def) {
+		std::cout << "Число является счастливым";
+	}
+	else {
+		std::cout << "Число не является счастливым";
+	}
+}
